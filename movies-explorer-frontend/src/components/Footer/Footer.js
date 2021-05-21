8246a711165b9ca7af66403365423d00c028ca52
Maxim <samai2@yandex.ru>
2021-05-21 19:56:05 +0300

    landing-adaptive

--- movies-explorer-frontend/src/components/Footer/Footer.js
+++ movies-explorer-frontend/src/components/Footer/Footer.js
@@ -1,6 +1,4 @@
-import {
-  Switch, Route, useParams, Link, NavLink
-} from "react-router-dom";
+
 
 import React from "react";
 import './Footer.css';
@@ -8,15 +6,16 @@ import './Footer.css';
 function Footer(props) {
   return (
       <footer className={'footer'}>
-        <div className={'footer__wrap'}>
-  <h4 className={'footer__title'}>Учебный проект Яндекс.Практикум х BeatFilm.</h4>
-          <p className={'footer__copy'}>&copy;2021</p>
-          <ul className='footer__links'>
-            <li className={'footer__linkItem'}><Link to={'#'} className={'footer__link'}>Яндекс.Практикум</Link></li>
-            <li className={'footer__linkItem'}><Link to={'#'} className={'footer__link'}>Github</Link></li>
-            <li className={'footer__linkItem'}><Link to={'#'} className={'footer__link'}>Facebook</Link></li>
-
-          </ul>
+        <div className={'footer__wrap section__wrap'}>
+          <h4 className={'footer__title'}>Учебный проект Яндекс.Практикум х BeatFilm.</h4>
+          <div className={'footer__flexWrap'}>
+            <p className={'footer__copy'}>&copy;2021</p>
+            <ul className='footer__links'>
+              <li className={'footer__linkItem'}><a href='#' className={'footer__link'} rel='noreferrer'>Яндекс.Практикум</a></li>
+              <li className={'footer__linkItem'}><a href={'#'} className={'footer__link'} rel='noreferrer'>Github</a></li>
+              <li className={'footer__linkItem'}><a href={'#'} className={'footer__link'} rel='noreferrer'>Facebook</a></li>
+            </ul>
+          </div>
         </div>
       </footer>
   )
