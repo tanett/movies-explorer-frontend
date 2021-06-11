a00f4187bc668214a9cd603092c3205b899c2eab
Maxim <samai2@yandex.ru>
2021-06-11 12:54:18 +0300

    header -fix

--- movies-explorer-frontend/src/components/Header/Header.js
+++ movies-explorer-frontend/src/components/Header/Header.js
@@ -20,44 +20,41 @@ function Header(props) {
       <header className={` header ${mobMenu ? "header_mobileOpen" : ""}`}>
         <div className='section__wrap header__wrap'>
           <Link to='/' className='header__logo'/>
-          <Switch>
-
-            <Route exact path='/'>
-              <nav className='header__nav '>
+          {(!props.loggedIn) ?
+              (<nav className='header__nav '>
                 <Link to='/signup' className='header__linkSignin'>
                   Регистрация
                 </Link>
-                <Link to={props.loggedIn? '/movies':'/signin'} className='header__linkSignin header__btn'>
+                <Link to={props.loggedIn ? '/movies' : '/signin'} className='header__linkSignin header__btn'>
                   Войти
                 </Link>
-              </nav>
-            </Route>
-            <Route exact path={'/movies' | '/saved-movies' | '/profile'}>
-              <button className={`${mobMenu ? "header__mobMenu_open" : "header__mobMenu"}`}
-                      onClick={handleClicMobMenu}/>
-              <div className={`header__linkWrap ${mobMenu ? "header__linkWrap_mobileMenuOpen" : ""}`}>
-                <button className={`${mobMenu ? "header__closeMobMenuBtn" : "header__closeMobMenuBtn_hidden"}`}
-                        onClick={handleClose}/>
-                <nav className={`header__nav ${mobMenu ? "header__nav_mobileMenuOpen" : ""}`}>
-                  <NavLink exact to='/' className={`${mobMenu ? "header__link" : "header__link_hidden"}`}
-                           activeClassName='header__link_active' onClick={handleClose}>
-                    Главная
-                  </NavLink>
-                  <NavLink exact to='/movies' className='header__link' activeClassName='header__link_active'
-                           onClick={handleClose}>
-                    Фильмы
-                  </NavLink>
-                  <NavLink to='/saved-movies' className='header__link' activeClassName='header__link_active'
-                           onClick={handleClose}>
-                    Сохраненные фильмы
-                  </NavLink>
-                </nav>
-                <Link to='/profile' className='header__btnAcc' onClick={handleClose}>
-                  <span className='header__btnAccText'>Аккаунт</span>
-                </Link>
-              </div>
-            </Route>
-          </Switch>
+              </nav>) : (
+                  <>
+                    <button className={`${mobMenu ? "header__mobMenu_open" : "header__mobMenu"}`}
+                            onClick={handleClicMobMenu}/>
+                    <div className={`header__linkWrap ${mobMenu ? "header__linkWrap_mobileMenuOpen" : ""}`}>
+                      <button className={`${mobMenu ? "header__closeMobMenuBtn" : "header__closeMobMenuBtn_hidden"}`}
+                              onClick={handleClose}/>
+                      <nav className={`header__nav ${mobMenu ? "header__nav_mobileMenuOpen" : ""}`}>
+                        <NavLink exact to='/' className={`${mobMenu ? "header__link" : "header__link_hidden"}`}
+                                 activeClassName='header__link_active' onClick={handleClose}>
+                          Главная
+                        </NavLink>
+                        <NavLink exact to='/movies' className='header__link' activeClassName='header__link_active'
+                                 onClick={handleClose}>
+                          Фильмы
+                        </NavLink>
+                        <NavLink to='/saved-movies' className='header__link' activeClassName='header__link_active'
+                                 onClick={handleClose}>
+                          Сохраненные фильмы
+                        </NavLink>
+                      </nav>
+                      <Link to='/profile' className='header__btnAcc' onClick={handleClose}>
+                        <span className='header__btnAccText'>Аккаунт</span>
+                      </Link>
+                    </div>
+                  </>)}
+
         </div>
       </header>
   )
